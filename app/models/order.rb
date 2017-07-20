79d87944d2ae4def9d78a42914627a393794591d
李阳 <admin@admindeMacBook-Air.local>
2017-07-21 00:01:27 +0800

    add token to order

--- app/models/order.rb
+++ app/models/order.rb
@@ -7,4 +7,10 @@ class Order < ApplicationRecord
   validates :billing_address,presence: true
   validates :shipping_name,presence: true
   validates :shipping_address,presence:true
+
+  before_action :generate_token
+
+  def generate_token
+    self.token = SecureRandom.uuid
+  end
 end
